fd8d4517fab4bac5ef64797ff76f379ee06ee0a5
StephanGerbeth <stephan.gerbeth@gmail.com>
2020-06-08 11:01:28 +0200

    feat(video): added custom video

---
+++ lib/components/CustomVideo.vue
@@ -0,0 +1,15 @@
+<template>
+  <video v-bind="$attrs">
+    <source>
+  </video>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="postcss" scoped>
+/* video {} */
+</style>
